dbe32462a222b4db187f352909462a0a95cc3428
eldop <nik1101810@mail.ru>
2020-04-18 15:39:25 +0300

    baloons

---
+++ lint-prov.py
@@ -0,0 +1,17 @@
+from math import *
+import itertools
+
+def  CalculateSquareRoot (Number ):
+	return  sqrt(Number )
+
+def append_item(item, l=[]):
+    l.append(item)
+    return l
+
+while True:
+    try:
+        your_number=float(input('Enter your number: '))
+        print("Square root is:", CalculateSquareRoot(your_number))
+        break
+    except:
+        pass
